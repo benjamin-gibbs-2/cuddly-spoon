df6030f3ef294b91dd38e2e833754af35f909172
benjamin-gibbs-2 <benjamin.gibbs2@gmail.com>
2022-05-03 12:38:55 -0400

    Test copy

---
+++ caesarCipherDecryption.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class caesarCipherDecryption {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Input the ciphered message : ");
+        String ciphertext = sc.nextLine();
+        System.out.println(" Enter the shift value :");
+        int shiftInt = sc.nextInt();
+        String decryptMessage = "";
+        for(int i=0; i < ciphertext.length();i++){
+            char alphabet =ciphertext.charAt(i);
+            if(alphabet >= 'a' && alphabet <= 'z'){
+                alphabet = (char)(alphabet - shiftInt);
+
+                if(alphabet < 'a'){
+                    alphabet = (char)(alphabet-'a'+'z'+1);
+                }
+                decryptMessage = decryptMessage + alphabet;
+            }
+            else if(alphabet >='A' && alphabet <='Z'){
+                alphabet =(char)(alphabet-shiftInt);
+                if (alphabet < 'A') {
+
+                    alphabet = (char)(alphabet-'A'+'Z'+1);
+                }
+                decryptMessage = decryptMessage + alphabet;
+            }
+            else{
+                decryptMessage = decryptMessage + alphabet;
+            }
+        }
+        System.out.println(" decrypt message : " + decryptMessage);
+    }
+}
